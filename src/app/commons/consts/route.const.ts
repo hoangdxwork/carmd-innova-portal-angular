9a10f4d9cc288398560cb4a948c546cf41ce2b12
hoangdxwork <hoangdx.work@gmail.com>
2024-08-27 16:31:53 +0700

    add login page

--- src/app/commons/consts/route.const.ts
+++ src/app/commons/consts/route.const.ts
@@ -1,13 +1,122 @@
+export interface RouteSubInfo {
+    path: string;
+    title: string;
+    class: string;
+}
+
 export interface RouteInfo {
     path: string;
     title: string;
     icon: string;
     class: string;
+    sub?: RouteSubInfo[];
 }
 
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard',       title: 'Dashboard',         icon: 'nc-bank',                class: '' },
-    { path: '/icon',            title: 'Icon',              icon: 'nc-bank',                class: '' },
-    { path: '/ui',              title: 'UI',                icon: 'nc-bank',                class: '' },
-    { path: '/setting',         title: 'Setting',           icon: 'nc-settings-gear-65',    class: 'active-pro' },
+    {
+        path:   '/dashboard',
+        title:  'Dashboard',
+        icon:   'bx bx-grid-alt',
+        class:  'active-pro'
+    },
+    {
+        path:   '/category',
+        title:  'Category',
+        icon:   'bx bx-collection',
+        class:  '',
+        sub:    [
+            {
+                path: '/category/html-css',
+                title: 'HTML & CSS',
+                class: ''
+            },
+            {
+                path: '/category/javaScript',
+                title: 'JavaScript',
+                class: ''
+            },
+            {
+                path: '/category/php-mysql',
+                title: 'PHP & MySQL',
+                class: ''
+            }
+        ]
+    },
+    {
+        path:   '/posts',
+        title:  'Posts',
+        icon:   'bx bx-book-alt',
+        class:  '',
+        sub:    [
+            {
+                path: '/posts/web-design',
+                title: 'Web Design',
+                class: ''
+            },
+            {
+                path: '/posts/login-form',
+                title: 'Login Form',
+                class: ''
+            },
+            {
+                path: '/posts/card-design',
+                title: 'Card Design',
+                class: ''
+            }
+        ]
+    },
+    {
+        path:   '/analytics',
+        title:  'Analytics',
+        icon:   'bx bx-pie-chart-alt-2',
+        class:  ''
+    },
+    {
+        path:   '/chart',
+        title:  'Chart',
+        icon:   'bx bx-line-chart',
+        class:  ''
+    },
+    {
+        path:   '/plugins',
+        title:  'Plugins',
+        icon:   'bx bx-plug',
+        class:  '',
+        sub:    [
+            {
+                path: '/posts/ui-face',
+                title: 'UI Face',
+                class: ''
+            },
+            {
+                path: '/posts/pigments',
+                title: 'Pigments',
+                class: ''
+            },
+            {
+                path: '/posts/box-icons',
+                title: 'Box Icons',
+                class: ''
+            }
+        ]
+    },
+    {
+        path:   '/explore',
+        title:  'Explore',
+        icon:   'bx bx-compass',
+        class:  ''
+    },
+    {
+        path:   '/history',
+        title:  'History',
+        icon:   'bx bx-history',
+        class:  ''
+    },
+    {
+        path:   '/setting',
+        title:  'Setting',
+        icon:   'bx bx-cog',
+        class:  ''
+    }
 ];
+
---
+++ src/app/components/authentication/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    selector: 'app-login',
+    templateUrl: './login.component.html'
+  })
+export class LoginComponent {
+  
+    constructor() { }
+  
+}
